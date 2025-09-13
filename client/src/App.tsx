23e86bcffe9300fbf6b79f6f9a5be23361155ee0
Tashdik <s2110477153@ru.ac.bd>
2025-09-13 16:55:12 +0600

    client folder and initial setup complete

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="text-5xl text-slate-500 font-extrabold flex justify-center items-center h-screen">
+      Team Cypher
+    </div>
+  );
+};
+
+export default App;
